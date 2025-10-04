d8e420cfe629ef1848620f4e3b27119d7687187b
pablo10diez2 <pablo.diez@opendeusto.es>
2025-10-04 12:03:26 +0200

    globo terraqueo con leaflet

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
-import './style.css';
-import * as Cesium from 'cesium';
-window.Cesium = Cesium;
-
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-import {fromLonLat} from 'ol/proj';
-
-import OLCesium from 'ol-cesium';
-
-const map = new Map({
-target: 'map',
-    layers: [
-        new TileLayer({
-            source: new OSM()
-        })
-    ],
-    view: new View({
-        center: fromLonLat([0, 0]),
-        zoom: 4
-    })
+var map = L.map('map', {
+    center: [40.4168, -3.7038],
+    zoom: 5,
+    zoomControl: false,       
+    attributionControl: false,
+    dragging: true,           
+    scrollWheelZoom: true,    
+    doubleClickZoom: false,   
+    boxZoom: false,           
+    keyboard: false           
 });
 
-const ol3d = new OLCesium({map: map});
-ol3d.setEnabled(true);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    subdomains: 'abcd',
+    attribution: '&copy; CARTO'
+}).addTo(map);
+
+var lugares = [
+    { nombre: "Museo del Prado", lat: 40.4138, lng: -3.6921, info: "Arte clásico y moderno" },
+    { nombre: "Parque del Retiro", lat: 40.4153, lng: -3.6845, info: "Parque urbano con estanque" },
+    { nombre: "Puerta del Sol", lat: 40.4169, lng: -3.7038, info: "Centro de Madrid" }
+];
+
+lugares.forEach(function(lugar) {
+    var marker = L.marker([lugar.lat, lugar.lng]).addTo(map);
+    marker.bindPopup("<b>" + lugar.nombre + "</b><br>" + lugar.info);
+});
